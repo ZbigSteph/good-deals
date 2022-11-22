20bd809487fe36da734721e98d87ad8c6962668e
TheBigSteph <nzali.stephane86@gmail.com>
2022-11-22 08:54:54 +0100

    update article json rendered

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,15 +5,15 @@ class ArticlesController < ApplicationController
   # GET /articles or /articles.json
   def index
     @articles = Article.all
-    @articles.joins(:image_couverture)
-    @articles.joins(:autres_images)
-    render json: @articles.map { |article|
-      article.as_json(only: %i[article.category label description prix
-                               partner]).merge(image_couverture_path: polymorphic_url(article.image_couverture))
-             .merge(autre_images_path: article.autres_images.each do |img|
-                                         polymorphic_url(img)
-                                       end)
-    }
+    # @articles.joins(:image_couverture)
+    # @articles.joins(:autres_images)
+    # render json: @articles.map { |article|
+    #   article.as_json(only: %i[article.category label description prix
+    #                            partner]).merge(image_couverture_path: polymorphic_url(article.image_couverture))
+    #          .merge(autre_images_path: article.autres_images.each do |img|
+    #                                      polymorphic_url(img)
+    #                                    end)
+    # }
   end
 
   # GET /articles/1 or /articles/1.json
--- app/views/articles/_article.json.jbuilder
+++ app/views/articles/_article.json.jbuilder
@@ -1,4 +1,7 @@
 json.extract! article, :id, :category, :label, :description, :prix, :autres_images, :partner
 # json.url article_url(article, format: :json)
 json.image_url polymorphic_url(article.image_couverture) if article.image_couverture.attached?
-
+json.attachments article.attachments do |attachment|
+  json.filename attachment.filename
+  json.url url_for(attachment)
+end
