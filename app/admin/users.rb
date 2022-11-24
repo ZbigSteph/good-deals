3a47e5b88614b8cede945e4bc94974cc08573380
TheBigSteph <nzali.stephane86@gmail.com>
2022-11-24 06:01:18 +0100

    update

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -7,5 +7,17 @@ ActiveAdmin.register User do
   #
   permit_params :nom, :prenom, :email, :role, :telephone, :password, :password_confirmation
   
+  form do |f| 
+    f.inputs do
+      f.input :nom
+      f.input :prenom
+      f.input :telephone
+      f.input :email
+      f.input :role
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
   
 end
