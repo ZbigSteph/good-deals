99c03fd4c986b87cee4b3c90f9aa5fb43ec76c47
TheBigSteph <nzali.stephane86@gmail.com>
2022-12-29 12:32:28 +0100

    update

--- app/controllers/sous_categories_controller.rb
+++ app/controllers/sous_categories_controller.rb
@@ -1,10 +1,22 @@
 class SousCategoriesController < InheritedResources::Base
-  skip_before_action :authenticate_request
+  skip_before_action :authenticate_request, only: %i[index show]
+  before_action :set_sous_category, only: %i[show edit update destroy]
+
+  # GET /categories or /categories.json
+  def index
+    @sous_categories = SousCategory.all
+  end
+
+  # GET /categories/1 or /categories/1.json
+  def show; end
 
   private
 
-    def sous_category_params
-      params.require(:sous_category).permit(:titre, :category_id)
-    end
+  def set_sous_category
+    @sous_category = SousCategory.find(params[:id])
+  end
 
+  def sous_category_params
+    params.require(:sous_category).permit(:titre, :category_id)
+  end
 end
