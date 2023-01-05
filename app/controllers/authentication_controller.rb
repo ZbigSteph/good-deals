11e9f92026ade8c72d0270623259248d652dda1c
TheBigSteph <nzali.stephane86@gmail.com>
2023-01-05 09:36:04 +0100

    update login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,13 +3,20 @@ class AuthenticationController < ApplicationController
 
   # POST auth/login
   def login
-    @user = User.find_by(email: params[:email])
-    if @user&.authenticate(params[:password])
-      #token = jwt_encode(user_id: @user.id)
+    # @user = User.find_by(email: params[:email])
+    # if @user&.authenticate(params[:password])
+    #   #token = jwt_encode(user_id: @user.id)
 
-      render json: { data: @user, status: true }, status: :ok
+    #   render json: { data: @user, status: true }, status: :ok
+    # else
+    #   render json: { error: 'unauthorized', authenticate: false }, status: :unauthorized
+    # end
+    @partner = Partner.find_by(telephone: params[:telephone])
+    if @partner.present?
+      render json: @partner
     else
-      render json: { error: 'unauthorized', authenticate: false }, status: :unauthorized
+      render json: { error: 'Utilisateur non existant' }
     end
   end
+  
 end
