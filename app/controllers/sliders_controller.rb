860491256bbb3f0f10c126ed75f41664ad97fc02
TheBigSteph <nzali.stephane86@gmail.com>
2023-08-09 21:14:48 +0100

    add slider module add more update

--- app/controllers/sliders_controller.rb
+++ app/controllers/sliders_controller.rb
@@ -1,4 +1,9 @@
 class SlidersController < InheritedResources::Base
+  skip_before_action :authenticate_request, only: %i[create index show articles_by_partner]
+
+  def index
+    @sliders = Slider.all
+  end
 
   private
 
